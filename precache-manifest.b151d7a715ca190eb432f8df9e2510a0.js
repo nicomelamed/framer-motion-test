90d7e0e126c34de9931c907bb689dbb2f7203526
nmelamed <nmelamed@glamit.com.ar>
2020-07-25 15:34:14 -0300

    Updates

---
+++ precache-manifest.b151d7a715ca190eb432f8df9e2510a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c97195a6a163b5b1de0d3148adb48fc",
+    "url": "/framer-motion-test/index.html"
+  },
+  {
+    "revision": "806991a1ef251536d0e0",
+    "url": "/framer-motion-test/static/css/main.523a06b0.chunk.css"
+  },
+  {
+    "revision": "3bf6eb80d02e2848fc5f",
+    "url": "/framer-motion-test/static/js/2.b0abc458.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/framer-motion-test/static/js/2.b0abc458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806991a1ef251536d0e0",
+    "url": "/framer-motion-test/static/js/main.e65c048c.chunk.js"
+  },
+  {
+    "revision": "49136b6fc62bd2200b45",
+    "url": "/framer-motion-test/static/js/runtime-main.4ceec425.js"
+  },
+  {
+    "revision": "d52214a098b81b6c97bc188ba973123c",
+    "url": "/framer-motion-test/static/media/background.d52214a0.jpg"
+  }
+]);
